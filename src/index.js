ebe5515fe2a45e3890f1b7d108c0ec146bb84824
elmar <mehdiyev@aitest.vinnova.de>
2022-02-24 17:23:45 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/app/app';
+import './index.css';
+// import AppHeader from './components/app-header/app-header'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
